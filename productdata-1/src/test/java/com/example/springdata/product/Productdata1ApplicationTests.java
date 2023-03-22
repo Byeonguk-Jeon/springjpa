fdb324f05006309b5aebcb6499d6f3511b72ac79
kimmingye <dusgml3779@gmail.com>
2023-03-22 18:39:53 +0900

    select update delete jpa

--- productdata-1/src/test/java/com/example/springdata/product/Productdata1ApplicationTests.java
+++ productdata-1/src/test/java/com/example/springdata/product/Productdata1ApplicationTests.java
@@ -14,28 +14,51 @@ class Productdata1ApplicationTests {
 
 	@Autowired
 	ProductRepository repository;
-	
+
 	@Test
 	void contextLoads() {
 	}
-	
+
 	@Test
 	public void testCreate() {
 		Product product = new Product();
-		product.setId(2);
+		product.setId(1);
 		product.setName("Iphone");
 		product.setDes("Awesome");
 		product.setPrice(1000d);
-		
+
 		repository.save(product);
-		
+
 	}
-	
+
 	@Test
 	public void testRead() {
 		Product product = repository.findById(1).get();
-        System.out.println(product.toString());
-		
+		System.out.println(product.toString());
+
 	}
 
+	@Test
+	public void testUpdate() {
+		Product product = repository.findById(1).get();
+		product.setPrice(1200d);
+		repository.save(product);
+	}
+
+	@Test
+	public void testDelete() {
+
+		if (repository.existsById(1)) {
+
+			repository.deleteById(1);
+
+		}
+	}
+
+	@Test
+	public void testCount() {
+
+		System.out.println(repository.count());
+
+	}
 }
